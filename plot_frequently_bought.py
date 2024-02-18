6762f5c89f5ca2867f70e97250e8fa8fbfc82db4
bryanpiguave <bpiguave@espol.edu.ec>
2024-02-18 10:07:41 -0500

    plot frequently bought together

--- plot_frequently_bought.py
+++ plot_frequently_bought.py
@@ -1,13 +1,14 @@
 import argparse
 import networkx as nx
 import plotly.graph_objects as go
-import json
+import os
 import matplotlib.pyplot as plt
+from src.plots.subgraph_plotly import plot_subgraph_plotly 
 parser=argparse.ArgumentParser()
 parser.add_argument('--graph_path', help='Path to the graph', default='graph.gml')
 parser.add_argument('--product_dict_path', help='Path to the product dictionary', default='product_dict.json')
-parser.add_argument('--output_path', help='Path to the output', default='subgraph.html')
-parser.add_argument('--center_node', help='Center node',default='PRD0904995')
+parser.add_argument('--output_path', help='Path to the output')
+parser.add_argument('--center_node', help='Center node',default='PRD0903052')
 args=parser.parse_args()
 # Create a sample graph
 G = nx.read_gml(args.graph_path)
@@ -31,31 +32,62 @@ def subgraph_frequently_together(graph:nx.classes.graph.Graph,
     """
 
     # Compute the maximum weight within radius 2 from the query node
-    ego = nx.ego_graph(graph, center_node, radius=2)
+    ego = nx.ego_graph(graph, center_node, radius=radius)
     max_weight = max(ego.edges[u, v]['weight'] for u, v in ego.edges())
-    # Get the nodes with the largest weight within radius 2
+    # Get the nodes with the largest weight within a radius
     nodes_with_max_weight = [center_node] + [node for node in ego.nodes() if any(ego.edges[node, neighbor]['weight'] >= max_weight for neighbor in ego.neighbors(node))]
     # Construct the subgraph
     subgraph = graph.subgraph(nodes_with_max_weight)
     return subgraph
 
 # Construct the subgraph
-subgraph = subgraph_frequently_together(G, query_node, radius=2)
+radius = 3
+subgraph = subgraph_frequently_together(G, query_node, radius=radius)
 # Draw the subgraph
 pos = nx.random_layout(subgraph)  # Use random layout
 # Highlight the query node
 plt.figure()
-plt.title("Products within a radius of 2 from node {}".format(query_node))
+plt.title("Products within a radius of {} from node {}".format(radius,query_node))
 nx.draw(subgraph, pos, with_labels=True, node_size=700, node_color='skyblue', font_size=10, font_weight='bold', edge_color='gray', width=1)
 nx.draw_networkx_nodes(subgraph, pos, nodelist=[query_node], node_color='red', node_size=700)
 plt.tight_layout()
 # Display the plot
-plt.savefig("subgraph.png")
+if args.output_path:
+    plt.savefig(args.output_path)
+else:
+    os.makedirs('visualization', exist_ok=True)
+    plt.savefig("visualization/subgraph.png")
 
+def subgraph_top_nodes_with_largest_weights(graph, query_node, top_n=10):
+    # Get the neighbors of the query node and their weights
+    neighbor_weights = [(neighbor, graph.edges[query_node, neighbor]['weight']) for neighbor in graph.neighbors(query_node)]
+    
+    # Sort the neighbors by weight in descending order and get the top n
+    top_nodes = [neighbor for neighbor, weight in sorted(neighbor_weights, key=lambda x: x[1], reverse=True)[:top_n]]
+    
+    # Construct the subgraph with the query node and top nodes
+    nodes_of_interest = [query_node] + top_nodes
+    subgraph = graph.subgraph(nodes_of_interest)
+    
+    return subgraph
 
 # Get the neighbors of the query node and their weights
 neighbor_weights = [(neighbor, G.edges[query_node, neighbor]['weight']) for neighbor in G.neighbors(query_node)]
-# Sort the neighbors by weight in descending order and get the top 10
-top_10_nodes = [neighbor for neighbor, weight in sorted(neighbor_weights, key=lambda x: x[1], reverse=True)[:10]]
+top_10_nodes = sorted(neighbor_weights, key=lambda x: x[1], reverse=True)[:10]
+
+# Print the top 10 nodes and their corresponding weights
 print("Top 10 nodes with largest weights connected to node", query_node, ":")
-print("{}".format("\n ".join(str(node) for node in top_10_nodes)))
+for node, weight in top_10_nodes:
+    print("Node:", node, "Weight:", weight)
+
+
+subgraph = subgraph_top_nodes_with_largest_weights(G, query_node, top_n=10)
+plot_subgraph_plotly(subgraph, query_node, args, radius=1)
+
+degrees = dict(G.degree())
+
+# Find the node with the maximum degree
+most_connected_node = max(degrees, key=degrees.get)
+
+print("Most connected node:", most_connected_node)
+print("Degree:", degrees[most_connected_node])
